332f6c90059631172d3e969ef51116cabbe1b1ac
Selethen <lukisd@op.pl>
2019-11-06 13:49:02 +0100

    Added skeleton of a class with shortcuts transformations.

---
+++ src/ShortcutTransformation.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class ShortcutTransformation {
+
+    private static HashMap<String, String> shortcuts = new HashMap<String, String>() {{
+        put("np.", "na przykład");
+        put("m.in", "między innymi");
+        put("itd.", "i tym podobne");
+        put("prof.", "profesor");
+        put("dr", "doktor");
+    }};
+
+    private static String transform(String text) {
+        //Pattern pattern = Pattern.compile("[REG-EXP]");
+        // pattern.matcher("tekst").matches();
+        // żeby nie zwracać uwagi na wielkość liter ?
+        return shortcuts.get(text);
+    };
+
+    /**
+     * Load from file predefined shortcuts.
+     */
+    private static void LoadFromFile() {
+
+    }
+}
