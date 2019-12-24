56e82131a6ea3592868e7dcc8b486b042fe98844
Selethen <lukisd@op.pl>
2019-12-24 16:54:58 +0100

    Added text transformation tests.

--- src/test/java/main_package/TransformationDecoratorTest.java
+++ src/test/java/main_package/TransformationDecoratorTest.java
@@ -1,61 +1,77 @@
 package main_package;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class TransformationDecoratorTest {
+public class TransformationDecoratorTest {
     private Transformation t;
 
     @BeforeEach
-    void setUp() {
+    public void setup() {
         t = new SimpleTransformation();
     }
-     /* 0: upper text
-     * 1: lower text
-     * 2: inverse text
-     * 3: capital letters
-     * 4: without repetitions
-     */
     @Test
-    public void textUpperTest() {
+    public void upperTest() {
         t = new TextTransformation(t);
+        String[] predicted = new String[3];
+        String[] result =  {"ŹREBIĘ", "MŁODE ŹREBIĘ", "ŹREBIĘ-012"};
 
-        String[] predict = new String[3];
-        String[] result = new String[3];
+        predicted[0] = t.transform("ŹREBIĘ", 0);
+        predicted[1] = t.transform("młode źrebię", 0);
+        predicted[2] = t.transform("ŹrEbIĘ-012", 0);
 
-        predict[0] = t.transform("WIELKIE",0);
-        result[0] = "WIELKIE";
-
-        predict[1] = "małe";
-        result[1] = "MAŁE";
-
-        predict[2] = "źrEbiĘ";
-        result[2] = "ŹREBIĘ";
-
-        Assert.assertArrayEquals(predict, result);
+        Assert.assertArrayEquals(result, predicted);
     }
-
     @Test
-    public void textLowerTest() {
+    public void lowerTest() {
+        t = new TextTransformation(t);
+        String[] predicted = new String[3];
+        String[] result =  {"źrebię", "młode źrebię", "źrebię-012"};
 
+        predicted[0] = t.transform("ŹREBIĘ", 1);
+        predicted[1] = t.transform("młode źrebię", 1);
+        predicted[2] = t.transform("ŹrEbIĘ-012", 1);
+
+        Assert.assertArrayEquals(result, predicted);
     }
-
     @Test
-    public void textCapitalTest() {
+    public void inverseTest() {
+        t = new TextTransformation(t);
+        String[] predicted = new String[3];
+        String[] result =  {"Ęiberź", "Edołm Ęiberź", "210-ęiberź"};
 
+        predicted[0] = t.transform("Źrebię", 2);
+        predicted[1] = t.transform("Młode Źrebię", 2);
+        predicted[2] = t.transform("źrebię-012", 2);
+
+        Assert.assertArrayEquals(result, predicted);
     }
+    @Test // change to delete space after
+    public void capitalTest() {
+        t = new TextTransformation(t);
+        String[] predicted = new String[3];
+        String[] result =  {"Źrebię ", "Młode Źrebię ", "Źrebię-012 "};
 
-    @Test
-    public void textInverseTest() {
+        predicted[0] = t.transform("źrebię", 3);
+        predicted[1] = t.transform("Młode źrebię", 3);
+        predicted[2] = t.transform("źrebię-012", 3);
 
+        Assert.assertArrayEquals(result, predicted);
     }
+    @Test // change to delete space after
+    public void repetitionTest() {
+        t = new TextTransformation(t);
+        String[] predicted = new String[3];
+        String[] result =  {"Spacer do sklepu ", "Spacer do sklepu po bułki ", "Myślę że masz rację "};
 
-    @Test
-    public void textDeleteSampleTest() {
+        predicted[0] = t.transform("Spacer do do do do sklepu", 4);
+        predicted[1] = t.transform("Spacer do do sklepu po po po bułki", 4);
+        predicted[2] = t.transform("Myślę że że masz masz rację", 4);
 
+        Assert.assertArrayEquals(result, predicted);
     }
-
 }
